f643cc9a8e40fdb0fd2174703d1cec94ad5f72cd
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-04-26 00:17:17 -0400

    added a connection to mysql, and git ignore file

--- employee_db.sql
+++ employee_db.sql
@@ -8,20 +8,31 @@ USE emp_db;
 -- Tables based on README
 
 CREATE TABLE department (
-id INTEGER auto_increment NOT NULL,
-name VARCHAR(30) NOT NULL,
-PRIMARY KEY (id)
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-id INTEGER auto_increment NOT NULL,
-title VARCHAR(30) NOT NULL,
-salary DECIMAL(2) NOT NULL,
-department_id INTEGER NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(2) NOT NULL,
+    department_id INTEGER NOT NULL,
 
 PRIMARY KEY(ID),
 );
 
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NOT NULL,
+    PRIMARY KEY(id)
+);
 
+SELECT * FROM employee;
+SELECT * FROM role;
+SELECT * FROM department;
 
 
